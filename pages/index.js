5590ddd9203fe2f02c7fa2ae96874e457923ba50
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-11-28 22:44:51 -0500

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+);
 
-
-export default function Home() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  )
-}
+export default Home;
