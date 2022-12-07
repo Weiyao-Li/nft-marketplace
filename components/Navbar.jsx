2c980b98e48d94cfa578ec19697faab5851188dc
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-12-07 18:35:48 -0500

    Navbar Done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -90,7 +90,7 @@ const Navbar = () => {
               alt="logo"
             />
             <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">
-              CryptoKet
+              NFT Marketplace
             </p>
           </div>
         </Link>
